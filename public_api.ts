bb9c1ba594898b689bf2e4a7107f4ef9c639f920
Sushant Pawar <sushant@townscript.com>
2019-02-20 18:24:44 +0530

    correct public_api

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,2 @@
-export * from './src/app/modules/ts-forms/ts-forms.module';
+export {TsFormsModule} from './src/app/modules/ts-forms/ts-forms.module';
 export {Option} from './src/app/modules/ts-forms/components/ts-select/ts-select.component';
