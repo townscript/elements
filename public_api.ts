89224a39b863fa46ad2854b347a6fe151cacd762
Pratik Patwari <pratik.patwari@townscript.com>
2019-07-19 18:39:20 +0530

    Added material dialog

--- public_api.ts
+++ public_api.ts
@@ -1,5 +1,6 @@
 export * from './src/app/core/index';
-export {Option} from './src/app/modules/ts-forms/components/ts-select/ts-select.component';
+export { Option } from './src/app/modules/ts-forms/components/ts-select/ts-select.component';
 export * from './src/app/modules/ts-forms/components/index';
 export * from './src/app/modules/ts-forms/ts-forms.module';
 export * from '@angular/material/bottom-sheet';
+export * from '@angular/material/dialog';
