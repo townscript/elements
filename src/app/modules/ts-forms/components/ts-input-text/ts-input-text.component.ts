dc3454fed161baa9314d980f6fc8a30ceda6bd9f
Swapnil Solanki <swapnil.solanki@townscript.com>
2020-06-29 12:21:30 +0530

    default floatLabel with input

--- src/app/modules/ts-forms/components/ts-input-text/ts-input-text.component.ts
+++ src/app/modules/ts-forms/components/ts-input-text/ts-input-text.component.ts
@@ -18,7 +18,7 @@ import { RegEx } from '../../../../shared/regex.constant';
 export class TsInputTextComponent extends TsControlValueAccessor implements OnInit {
 
   @Input() placeholder = '';
-  floatLabel = config.floatLabel;
+  @Input() floatLabel = config.floatLabel;
   @Input() required = false;
   @Input() disabled = false;
   @Input() name: string;
