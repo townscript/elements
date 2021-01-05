98e54ef9682dd3958ca47fb2c4e8cc2cff9bde98
Abhilasha <abhilasha.kumari@townscript.com>
2021-01-05 18:10:08 +0530

    file type input

--- src/app/modules/ts-forms/components/ts-input-file/ts-input-file.component.ts
+++ src/app/modules/ts-forms/components/ts-input-file/ts-input-file.component.ts
@@ -7,38 +7,52 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class TsInputFileComponent implements OnInit {
   constructor() { }
-  @Input() fileSize: number;
-  uploadPercentage = 50;
+  @Input() maxFileSize;
+  uploadPercentage: number;
   uploadedFile: any;
+  @Input() uploadComplete: boolean;
   @Output() fileValueChangeEvent = new EventEmitter<any>();
-
+  stopUploadBar = false;
 
   ngOnInit(): void {
-    this.updatePer();
   }
 
   emitFileValueChange = () => {
     this.fileValueChangeEvent.emit(this.uploadedFile);
   }
 
-  updatePer = () => {
-    //to emulate
-    setInterval(() => {
-      this.uploadPercentage += 1;
-    }, 1000);
-
-  }
-
   setUploadedFileValue = async (event: any): Promise<any> => {
     this.uploadedFile = undefined;
     this.uploadedFile = event.target.files[0];
+    if (this.uploadedFile.bytes > this.maxFileSize) {
+      alert('Sorry maximum file limit is ' + this.maxFileSize + ' KB');
+      this.uploadedFile = undefined;
+      return;
+    }
     this.emitFileValueChange();
+    this.emulateUpload();
   }
 
   deleteUploadedFileValue = () => {
     this.uploadedFile = undefined;
-    this.uploadPercentage = 0;
     this.emitFileValueChange();
+    this.stopUploadBar = true;
+  }
+
+  emulateUpload = () => {
+    this.stopUploadBar = false;
+    this.uploadPercentage = 0;
+    let current_progress = 0;
+    let step = 0.5;
+    const interval = setInterval(() => {
+      current_progress += step;
+      this.uploadPercentage = Math.round(Math.atan(current_progress) / (Math.PI / 2) * 100 * 1000) / 1000
+      if (this.uploadPercentage >= 100 || this.stopUploadBar || this.uploadComplete) {
+        clearInterval(interval);
+      } else if (this.uploadPercentage >= 70) {
+        step = 0.1
+      }
+    }, 100);
   }
 
 }
